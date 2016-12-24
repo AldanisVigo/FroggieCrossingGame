08b71262ffa7d2a5e723e7751f67699e3d85d233
Aldanis Vigo <aldanisvigo@gmail.com>
2016-12-23 22:14:51 -0600

    Initial Commit

---
+++ Froggie CrossingTests/Froggie_CrossingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Froggie_CrossingTests.swift
+//  Froggie CrossingTests
+//
+//  Created by Aldanis Vigo on 12/23/16.
+//  Copyright © 2016 Aldanis Vigo. All rights reserved.
+//
+
+import XCTest
+@testable import Froggie_Crossing
+
+class Froggie_CrossingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
